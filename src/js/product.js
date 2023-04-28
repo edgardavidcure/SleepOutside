1e8e82c3115836db9e75afa7a0405754e17a99b4
edgardavidcure <102256702+edgardavidcure@users.noreply.github.com>
2023-04-28 12:49:43 -0700

    Fixed issue with local storage that prevented the cart to get updated

--- src/js/product.js
+++ src/js/product.js
@@ -1,13 +1,22 @@
 import { setLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
+let products = [];
+console.log(products)
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  if(localStorage.getItem("so-cart")){
+		products = JSON.parse(localStorage.getItem("so-cart"));
+	}
+  products.push(product);
+  setLocalStorage("so-cart", products);
+
+  
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await findProductById(e.target.dataset.id);
   addProductToCart(product);
+
 }
 
 // add listener to Add to Cart button
