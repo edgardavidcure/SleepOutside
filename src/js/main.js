7b40b48ed26d329c4623a468c13672f6da214b3c
edgardavidcure <102256702+edgardavidcure@users.noreply.github.com>
2023-06-29 11:32:35 -0700

    completed trello card call to register

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,35 @@
 
-import { setSuperscript, loadHeaderFooter } from "./utils.mjs";
+import { setSuperscript, loadHeaderFooter, getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 
 loadHeaderFooter();
 
+document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.getElementById('myModal');
+    const closeButton = document.querySelector('.close');
+    const registerButton = document.getElementById('registerButton');
+  
+    const hasRegistered = getLocalStorage('registered');
+  
+    // If the visitor has not registered, show the modal
+    if (!hasRegistered) {
+      modal.style.display = 'block';
+    }
+  
+    // Close the modal when the close button is clicked
+    closeButton.addEventListener('click', () => {
+      modal.style.display = 'none';
+    });
+  
+    // Register button click event handler
+    registerButton.addEventListener('click', () => {
+      // Perform registration logic here
+      // ...
+  
+      setLocalStorage('registered', 'true');
+  
+      // Close the modal
+      modal.style.display = 'none';
+    });
+  });
+  
