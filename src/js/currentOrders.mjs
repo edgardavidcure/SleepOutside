9cffb47aea1943ada58f50265bd1fbcd51f983e6
Neolivan <neolivan.eng@outlook.com>
2023-06-30 17:41:31 -0300

    Merge ajusts

--- src/js/currentOrders.mjs
+++ src/js/currentOrders.mjs
@@ -23,19 +23,8 @@ export async function orders() {
           rows = "<tr>No old orders :(</tr>"  
         }
         table.innerHTML = rows;
-      
-      console.log(ordersData);
- 
-      const result = ordersData.reduce((groupedOrders, order) => {
-        const { fname, lname } = order;
-        const fullName = `${fname} ${lname}`;
-        
-        if (!groupedOrders[fullName]) {
-          groupedOrders[fullName] = [];
-        }
-        groupedOrders[fullName].push(order);
-        return groupedOrders;
-      }, {});
+    }
+  }
       
    
 
