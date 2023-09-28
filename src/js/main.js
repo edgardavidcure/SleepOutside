e6457c775e1568f1423dba35e24b0cad445561a4
edgardavidcure <102256702+edgardavidcure@users.noreply.github.com>
2023-09-28 11:23:21 -0700

    test

--- src/js/main.js
+++ src/js/main.js
@@ -8,34 +8,34 @@ import Alert from "./alerts";
 
 loadHeaderFooter();
 
-document.addEventListener("DOMContentLoaded", () => {
-  const modal = document.getElementById("myModal");
-  const closeButton = document.querySelector(".close");
-  const registerButton = document.getElementById("registerButton");
+// document.addEventListener("DOMContentLoaded", () => {
+//   const modal = document.getElementById("myModal");
+//   const closeButton = document.querySelector(".close");
+//   const registerButton = document.getElementById("registerButton");
 
-  const hasRegistered = getLocalStorage("registered");
+//   const hasRegistered = getLocalStorage("registered");
 
-  // If the visitor has not registered, show the modal
-  if (!hasRegistered) {
-    modal.style.display = "block";
-  }
+//   // If the visitor has not registered, show the modal
+//   if (!hasRegistered) {
+//     modal.style.display = "block";
+//   }
 
-  // Close the modal when the close button is clicked
-  closeButton.addEventListener("click", () => {
-    modal.style.display = "none";
-  });
+//   // Close the modal when the close button is clicked
+//   closeButton.addEventListener("click", () => {
+//     modal.style.display = "none";
+//   });
 
-  // Register button click event handler
-  registerButton.addEventListener("click", () => {
-    // Perform registration logic here
-    // ...
+//   // Register button click event handler
+//   registerButton.addEventListener("click", () => {
+//     // Perform registration logic here
+//     // ...
 
-    setLocalStorage("registered", "true");
+//     setLocalStorage("registered", "true");
 
-    // Close the modal
-    modal.style.display = "none";
-  });
-});
+//     // Close the modal
+//     modal.style.display = "none";
+//   });
+// });
 
 const newsletterForm = document.getElementById("news-signup");
 
