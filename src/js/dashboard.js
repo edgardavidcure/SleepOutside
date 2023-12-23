10fb16472b18dd0a420168b07050ff8e0078b6b9
edgar <edgardavid1503@gmail.com>
2023-12-22 20:41:22 -0800

    accessed cookie data and displayed user info

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -6,39 +6,33 @@ import {
 } from "./utils.mjs";
 import jwt_decode from "jwt-decode";
 const userNameEl = document.getElementById("userName");
+const jwt = getCookie("jwt");
 
-// function getUserInfo() {
-//   const token = getParam("token");
-//   const userInfo = jwt_decode(token);
-//   return userInfo;
-// }
-// const userInfo = getUserInfo();
-// console.log(userInfo);
-// userName.textContent = userInfo.displayName;
-// getUserInfo();
+function getCookie(cookieName) {
+  const name = cookieName + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const cookieArray = decodedCookie.split(";");
 
-async function getAuthHeader() {
-  try {
-    const response = await fetch("http://localhost:5173/dashboard/index.html");
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+  for (let i = 0; i < cookieArray.length; i++) {
+    let cookie = cookieArray[i].trim();
+    if (cookie.indexOf(name) === 0) {
+      return cookie.substring(name.length, cookie.length);
     }
-
-    const authHeader = response.headers.get("Authorization");
-
-    if (!authHeader) {
-      throw new Error("Authorization header not found in the response");
-    }
-
-    // Extract the token from the Authorization header
-    const authToken = authHeader.split(" ")[1];
-    console.log("Authorization Token:", authToken);
-
-    return authToken;
-  } catch (error) {
-    console.error("Error:", error);
   }
+
+  return null;
 }
 
-getAuthHeader();
+function getUserInfo() {
+  const userInfo = jwt_decode(jwt);
+  return userInfo;
+}
+const userInfo = getUserInfo();
+console.log(userInfo);
+userName.textContent = userInfo.displayName;
+getUserInfo();
+
+//things to do:
+//save the user data in local storage to display it across multiple pages
+//fetch user reviews
+//improve dashboard layout and design
